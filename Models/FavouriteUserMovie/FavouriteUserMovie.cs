ffe36de8cf81689f6e43af752d13a5e6bb22c0bf
19013083 <69584864+19013083@users.noreply.github.com>
2020-12-09 15:48:43 +0200

    favouriteUserMovie table added

---
+++ Models/FavouriteUserMovie/FavouriteUserMovie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace My_movie_manager.Models.FavouriteUserMovie
+{
+    [Keyless]
+    [Table("favouriteUserMovie")]
+    public partial class FavouriteUserMovie
+    {
+        [Column("User_id")]
+        public int? UserId { get; set; }
+        [Column("movieId")]
+        [StringLength(255)]
+        public string MovieId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; }
+    }
+}
