4b85acae8e3b1fa80208d5b0b14e804c54cc6647
19013083 <69584864+19013083@users.noreply.github.com>
2020-12-10 09:50:59 +0200

    Fav movie update

---
+++ Models/FavouriteUserMovie/FavouriteUserMovie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace My_movie_manager.Models.FavouriteUserMovie
+{
+    [Table("favouriteUserMovie")]
+    public partial class FavouriteUserMovie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column("User_Id")]
+        public int? UserId { get; set; }
+        [StringLength(255)]
+        public string MovieId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty("FavouriteUserMovies")]
+        public virtual User User { get; set; }
+    }
+}
--- SQL_table_creation.sql
+++ SQL_table_creation.sql
@@ -7,3 +7,9 @@
 	FavoriteMovie varchar(255),
 	DateOfBirth date
 );
+
+create table favouriteUserMovie(
+	Id int identity(1,1) not null primary key,
+	User_Id int FOREIGN KEY REFERENCES Users(User_Id),
+	MovieId varchar(255)
+);
